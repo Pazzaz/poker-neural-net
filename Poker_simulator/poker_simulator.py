393c30bc5436ea48db86453760614ad0838d9c07
Samuel Widén <samuel.widen@learnet.se>
2017-10-13 08:52:37 +0200

    Ändrade hand strength systemet för flushes då det var inkorrekt. Dessutom kan simulatorn nu hitta A-5 straight-flushes.

--- Poker_simulator/poker_simulator.py
+++ Poker_simulator/poker_simulator.py
@@ -37,8 +37,8 @@ def poker_simulator():
         flush_cards.sort()
         flush_cards.reverse()
         straight_flush_counter = 0
-        for a, b in zip(flush_cards[:-1], flush_cards[1:]): #<- Hur i hela friden funkar detta??
-            if a == b+1:
+        for a, b in zip(flush_cards[:-1], flush_cards[1:]):
+            if a == b+1 or (a == 12 and b == 3):
                 straight_flush_counter += 1
                 if straight_flush_counter == 4:
                     if flush_cards[0] == flush_cards[1]+1 and flush_cards[1] == flush_cards [2]+1:
@@ -48,20 +48,21 @@ def poker_simulator():
                     else:
                         straight_flush_strength = flush_cards[2]
                     player_1_hand_strength = [8, straight_flush_strength]
-                    print("Straight flush")
-                    print(sorted(player_1_hand))
-                    print(player_1_hand_strength)
-                    break
+                    # print ("Flush cards"+str(flush_cards))
+                    # print("Straight flush")
+                    # print(sorted(player_1_hand))
+                    # print(player_1_hand_strength)
+                    break #Break eftersom en straight flush upptäckts och vi inte behöver leta mer. 
             else:
                 straight_flush_counter = 0
         if int(player_1_hand_strength[0]) < 5:
             player_1_hand_strength[0] = 5
             # while len(flush_cards) > 5:
             #     flush_cards.pop()   
-            player_1_hand_strength[1] = sum(int(i) for i in flush_cards[0:6])
-            print("Flush")
-            print("Player 1 hand strength"+ str(player_1_hand_strength))
-            print("Flush cards"+ str(flush_cards))
+            # # player_1_hand_strength[1] = sum(int(i) for i in flush_cards[0:6])
+            # print("Flush")
+            # print("Player 1 hand strength"+ str(player_1_hand_strength))
+            # print("Flush cards"+ str(flush_cards))
 
         # if i[1] == 0:
         #     count_spades += 1
@@ -73,33 +74,6 @@ def poker_simulator():
         #     count_clubs += 1
         # if count_spades >= 5 or count_hearts >= 5 or count_diamonds >= 5 or count_clubs >= 5
 
-    # first_card = random.randint(0, 51)
-    # first_card_value = first_card % 13
-    # first_card_suit = first_card % 4
-    # cards_drawn.append(first_card)
-    # second_card = random.randint(0, 51)
-    # while second_card in cards_drawn:
-    #     second_card = random.randint(0, 51)
-    # second_card_value = second_card % 13
-    # second_card_suit = second_card % 4
-    # cards_drawn.append(second_card)
-    # third_card = random.randint(0, 51)
-    # while third_card in cards_drawn:
-    #     third_card = random.randint(0, 51)
-    # third_card_value = third_card % 13
-    # third_card_suit = third_card % 4
-    # cards_drawn.append(third_card)
-    # fourth_card = random.randint(0, 51)
-    # while fourth_card in cards_drawn:
-    #     fourth_card = random.randint(0, 51)
-    # fourth_card_value = fourth_card % 13
-    # fourth_card_suit = fourth_card % 4
-    # cards_drawn.append(fourth_card)
-    # player_1_cards = [[first_card_value, first_card_suit], [second_card_value, second_card_suit]]
-    # player_2_cards = [[third_card_value, third_card_suit], [fourth_card_value, fourth_card_suit]]
-    # player_1_cards_string = " ".join(str(x) for x in player_1_cards)
-    # player_2_cards_string = " ".join(str(x) for x in player_2_cards)
-    # output = "player_1_cards: " + player_1_cards_string + " player_2_cards: " + player_2_cards_string
     output = cards
     return output
     # return "player_1_cards: " + player_1_cards + "player_2_cards: " + player_2_cards
